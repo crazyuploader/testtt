407c7fb58aa806fe09234e542bc481fa5cea7e8f
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-03-13 21:09:55 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
+
+wget -nv https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r377782c.tar.gz
+
+ls -lh
