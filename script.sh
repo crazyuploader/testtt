86143daf303baf909acc412a973b979354c94b4a
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-03-14 20:41:55 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
 curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
-
-git clone https://gitlab.com/crazyuploader/clang-toolchain clang && cd clang && du -sh
