dbf2e20ddf1cfc37710039559a113f6923d2966c
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-03-14 20:07:09 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
+
+git clone https://gitlab.com/crazyuploader/clang-toolchain clang && cd clang && du -sh
