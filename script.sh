c36e3796c8e23a6cb718edba74507db8bf803a6d
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-04-11 19:06:00 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
 curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
-echo ""
-echo "TUCHIYA CHOOCHOOMAN"
-echo ""
-ping -c 10 flipkart.com
