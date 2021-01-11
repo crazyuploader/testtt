036ee4d736a28e1bebcf0f79a568abd4eca93399
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2021-01-11 14:57:45 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-git clone https://bitbucket.org/yalan_bb/internet_test_tools.git -b crazyuploader-dev
-cd ./internet_test_tools
-./networktest.sh
+docker run crazyuploader/internet_test_tools
