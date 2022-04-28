fa742cce0cca456b4ef2b436d8d21cb6b831676a
Jugal Kishore <me@devjugal.com>
2022-04-28 23:41:42 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-docker run crazyuploader/speedtest
+curl -sLo hey https://hey-release.s3.us-east-2.amazonaws.com/hey_linux_amd64
+chmod +x ./hey
+echo "Checking Performance for Server03"
+./hey -n 1000 https://server03.devjugal.com
+echo ""
+echo "Checking Performance for Server04"
+./hey -n 1000 https://server04.devjugal.com
