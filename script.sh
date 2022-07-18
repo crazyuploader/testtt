a0b0216cae617363c7cf99a2843865f7ec45136d
Jugal Kishore <me@devjugal.com>
2022-07-19 00:49:32 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,9 +2,8 @@
 
 curl -sLo hey https://hey-release.s3.us-east-2.amazonaws.com/hey_linux_amd64
 chmod +x ./hey
-curl -sL http://ip-api.com/json/
+curl -sL http://ip-api.com/json/ | jq
 echo ""
-echo "Checking Performance for Server03"
-./hey -n 1000000 https://server03.devjugal.com
+curl -sL api.devjugal.com/ip-info/ | jq
 echo "Checking Performance for Server04"
-./hey -n 1000000 https://server04.devjugal.com
+./hey -n 10000 https://server04.devjugal.com
